3858186e6fd5e48ea7c12030f86158801aff54c7
Alexander <112536439+avkravchenko@users.noreply.github.com>
2023-12-19 23:56:32 +0300

    добавил кеширование

--- src/api/loremPicsum.ts
+++ src/api/loremPicsum.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { setupCache } from "axios-cache-adapter";
 
 const cache = setupCache({
-  maxAge: 15 * 60 * 1000, // Cache responses for 15 minutes
+  maxAge: 15 * 60 * 1000,
   exclude: {
     query: false,
   },
